55f24e741c162943876e4b13776e3fe9d738e7b9
NastyaStaneva <86763482+NastyaStaneva@users.noreply.github.com>
2021-07-07 08:43:46 +0300

    Update save_users.php

--- Тест/regestration/save_users.php
+++ Тест/regestration/save_users.php
@@ -1,73 +1,61 @@
 <?php
-    //заносим введенную пользователем фамилию 
     if (isset($_POST['last_name'])) 
         { 
             $last_name = $_POST['last_name']; 
             if ($last_name =='') { unset($last_name);} 
         }
 
-    //заносим введенное пользователем имя
         if (isset($_POST['first_name'])) 
         { 
             $first_name = $_POST['first_name']; 
             if ($first_name =='') { unset($first_name);} 
         }
 
-    //заносим введенное пользователем отчество
         if (isset($_POST['patronymic'])) 
         { 
             $patronymic = $_POST['patronymic']; 
             if ($patronymic =='') { unset($patronymic);} 
         }
-
-    //заносим введенную пользователем школу
         if (isset($_POST['school'])) 
         { 
             $school = $_POST['school']; 
             if ($school =='') { unset($school);} 
         }
 
-    //заносим введенный пользователем класс
         if (isset($_POST['class'])) 
         { 
             $class = $_POST['class']; 
             if ($class =='') { unset($class);} 
         }
 
-    //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
         if (isset($_POST['email'])) 
         { 
             $login = $_POST['email']; 
             if ($login == '') { unset($login);} 
         } 
 
-    //заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
         if (isset($_POST['password'])) 
         { 
             $password = $_POST['password']; 
             if ($password =='') { unset($password);} 
         }
 
-     //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
          if (empty($last_name) or empty($first_name) or empty($patronymic) or empty($school) or empty($class) or empty($login) or empty($password))
         {
             exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
         }
 
-    //если логин и пароль введены, то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
+    
         $login = stripslashes($login);
         $login = htmlspecialchars($login);
         $password = stripslashes($password);
         $password = htmlspecialchars($password);
 
-    //удаляем лишние пробелы
         $login = trim($login);
         $password = trim($password);
 
-    // подключаемся к базе
         include ("dbconnect.php");
 
-    // проверка на существование пользователя с таким же логином
         $result = regestr_query("SELECT id FROM users WHERE email ='$login'",$db);
         $myrow = regestr_fetch_array($result);
         if (!empty($myrow['id'])) 
@@ -75,10 +63,8 @@
             exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
         }
 
-    // если такого нет, то сохраняем данные
         $result2 = regestr_query ("INSERT INTO users (email, password) VALUES('$login','$password')");
 
-    // Проверяем, есть ли ошибки
         if ($result2 =='TRUE')
         {
         echo "Вы успешно зарегистрированы! Теперь вы можете зайти на сайт. <a href='index.html'>Главная страница</a>";
