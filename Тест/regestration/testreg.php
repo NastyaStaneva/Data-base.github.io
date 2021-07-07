e7bcd13de716b97629c6efd4efb2787a8443b5a2
NastyaStaneva <86763482+NastyaStaneva@users.noreply.github.com>
2021-07-07 08:41:30 +0300

    Update testreg.php

--- Тест/regestration/testreg.php
+++ Тест/regestration/testreg.php
@@ -1,56 +1,42 @@
 <?php
-    session_start();//  вся процедура работает на сессиях. Именно в ней хранятся данные  пользователя, пока он находится на сайте. Очень важно запустить их в  самом начале странички!!!
-
-    //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
+    session_start();
     if (isset($_POST['email'])) 
         { 
             $login = $_POST['email']; 
             if ($login == '') 
                 { unset($login);} 
         } 
-
-    //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
     if (isset($_POST['password'])) 
         { 
             $password=$_POST['password']; 
             if ($password =='') 
                 { unset($password);} 
-        }
-    
-    //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+    }
     if (empty($login) or empty($password)) 
     {
         exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
     }
-
-    //если логин и пароль введены,то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
     $login = stripslashes($login);
     $login = htmlspecialchars($login);
     $password = stripslashes($password);
     $password = htmlspecialchars($password);
 
-    //удаляем лишние пробелы
     $login = trim($login);
     $password = trim($password);
 
-    // подключаемся к базе
     include ("dbconnect.php");
  
-    //извлекаем из базы все данные о пользователе с введенным логином
     $result = regestr_query("SELECT * FROM users WHERE email ='$login'",$db); 
     $myrow = regestr_fetch_array($result);
     if (empty($myrow['password']))
     {
-    //если пользователя с введенным логином не существует
         exit ("Извините, введённый вами логин или пароль неверный.");
     }
 
     else {
-    //если существует, то сверяем пароли
-    if ($myrow['password'] == $password) {
-    //если пароли совпадают, то запускаем пользователю сессию! 
+    if ($myrow['password'] == $password) { 
     $_SESSION['email'] = $myrow['email']; 
-    $_SESSION['id'] = $myrow['id'];//эти данные очень часто используются, вот их и будет "носить с собой" вошедший пользователь
+    $_SESSION['id'] = $myrow['id'];
     echo "Вы успешно вошли на сайт! <a href='index.html'>Главная страница</a>";
     }
     
