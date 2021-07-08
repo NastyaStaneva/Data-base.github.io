e7a54eabf33b70859046d6e457bed82aca42386e
NastyaStaneva <86763482+NastyaStaneva@users.noreply.github.com>
2021-07-08 08:29:16 +0300

    Update dbconnect.php

--- Тест/regestration/dbconnect.php
+++ Тест/regestration/dbconnect.php
@@ -1,4 +1,33 @@
 <?php
-        $db = regestr_connect ("localhost");
-        regestr_select_db ("regestr",$db);
+	//$db = mysqli_connect('localhost');
+	//$sel_db = mysqli_select_db(link $db, database_name 'registration');
+    //$db = regestr_connect ("localhost");
+    //regestr_select_db ("registration",$db);
 ?> 
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "registration";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, last_name, first_name,  FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
