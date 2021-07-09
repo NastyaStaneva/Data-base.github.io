c6594065921b1b7d7e49270702d8afd7703b7000
NastyaStaneva <86763482+NastyaStaneva@users.noreply.github.com>
2021-07-09 08:29:06 +0300

    Update dbconnect.php

--- Тест/regestration/dbconnect.php
+++ Тест/regestration/dbconnect.php
@@ -18,16 +18,24 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, last_name, first_name,  FROM users";
+//$sql = "SELECT id, last_name, firs_name,  FROM users";
+$sql = "SELECT * FROM users";
 $result = $conn->query($sql);
-
+/*
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["firs_name"]. " " . $row["last_name"]. "<br>";
   }
-} else {
-  echo "0 results";
+} 
+else {
+echo "0 results";
 }
-$conn->close();
+//$conn->close();*/
+?>
+
+<?php
+
+	/*require ('libs/rb.php'); //библиотека для соединения через readbeanphp
+	 R::setup( 'mysql:host=localhost;dbname=registration', 'root', '' );*/
 ?>
