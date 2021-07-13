eb4c14d2f8f7b0f3fe739da32b5e8f7f6704a342
NastyaStaneva <86763482+NastyaStaneva@users.noreply.github.com>
2021-07-13 16:53:44 +0300

    Add files via upload

--- test/test_my.php
+++ test/test_my.php
@@ -1,137 +1,77 @@
 <!DOCTYPE HTML>
 <?php 
-		$ls1 =  file("01.txt");  
-		$ls2 =  file("02.txt");
-		$ls3 =  file("03.txt");
-		$ls4 =  file("04.txt");
-		$ls5 =  file("05.txt");
-		$ls6 =  file("06.txt");
-		$ls7 =  file("07.txt");
-		$ls8 =  file("08.txt");
-		$ls9 =  file("09.txt");
-		$ls10 =  file("10.txt");
-		$ls11 =  file("11.txt");
-		$ls12 =  file("12.txt");
-		$ls13 =  file("13.txt");
-		$ls14 =  file("14.txt");
-		$ls15 =  file("15.txt");
-		$ls16 =  file("16.txt");
-		$ls17 =  file("17.txt");
-		$ls18 =  file("18.txt");
-		$ls19 =  file("19.txt");
-		$ls20 =  file("20.txt");
-		$ls21 =  file("21.txt");
+$lines = file('01.txt'); 
+  //-- проверяем на переход после тестирования
+    if( isset($_REQUEST['tst_nm'])){
 
-		$ls1_len = count($ls1);        // число строк в списке 
-		$ls2_len = count($ls2);        
-		$ls3_len = count($ls3);        
-		$ls4_len = count($ls4);        
-		$ls5_len = count($ls5);        
-		$ls6_len = count($ls6);        
-		$ls7_len = count($ls7);        
-		$ls8_len = count($ls8);        
-		$ls9_len = count($ls9);        
-		$ls10_len = count($ls10);        
-		$ls11_len = count($ls11);        
-		$ls12_len = count($ls12);        
-		$ls13_len = count($ls13);        
-		$ls14_len = count($ls14);        
-		$ls15_len = count($ls15);        
-		$ls16_len = count($ls16);       
-		$ls17_len = count($ls17);        
-		$ls18_len = count($ls18);        
-		$ls19_len = count($ls19);        
-		$ls20_len = count($ls20);        
-		$ls21_len = count($ls21);        
+      //-- формитуем полученные переменные
+        $tst_nm = $_REQUEST['tst_nm'];
+        //echo $tst_nm;
+      $itog_text = $_REQUEST['itog_text'];   
+      $fl_nm = $_REQUEST['fl_nm'];
+      //-- считываем файл с заданиями
+      //$lines  = file( $fl_nm);
+      echo $fl_nm;
+      echo $lines;
+      //-- формируем списки номеров заданий и ответов
+      $temp_list = explode(';', $itog_text);
+      //echo $itog_text;
+      $last_empty = array_pop($temp_list);
+      $quest_count = count($temp_list);
 
-		srand();                     // сбросить генератор случайных чисел
-		$num1 = rand(0, $ls1_len-1);   // получить случайный номер
-		$num2 = rand(0, $ls2_len-1);
-		$num3 = rand(0, $ls3_len-1);
-		$num4 = rand(0, $ls4_len-1);
-		$num5 = rand(0, $ls5_len-1);
-		$num6 = rand(0, $ls6_len-1);
-		$num7 = rand(0, $ls7_len-1);
-		$num8 = rand(0, $ls8_len-1);
-		$num9 = rand(0, $ls9_len-1);
-		$num10 = rand(0, $ls10_len-1);
-		$num11 = rand(0, $ls11_len-1);
-		$num12 = rand(0, $ls12_len-1);
-		$num13 = rand(0, $ls13_len-1);
-		$num14 = rand(0, $ls14_len-1);
-		$num15 = rand(0, $ls15_len-1);
-		$num16 = rand(0, $ls16_len-1);
-		$num17 = rand(0, $ls17_len-1);
-		$num18 = rand(0, $ls18_len-1);
-		$num19 = rand(0, $ls19_len-1);
-		$num20 = rand(0, $ls20_len-1);
-		$num21 = rand(0, $ls21_len-1);
+      $task_nums = array();
+      $answ_list = array();
+    foreach ($temp_list as $temp_line) {
+      $answ_pair = explode(':', $temp_line);
+      $task_nums[] = +$answ_pair[0];    //-- преобразовать строку в целое
+      $answ_list[] = trim($answ_pair[1]);  
+    }
 
-		$line1 = $ls1[$num1];
-		$pos = strrpos($line1, 'Ответ');
-		$true_answ[$line1] = substr(0, $pos);
+      //-- расчитать число правильных ответов
+    $itog_count = 0;
+    
+    for( $num=0; $num < count( $task_nums); $num++) {
+            $task_num = $task_nums[$num];
+      $line = $lines[$task_num - 1];
+      $line = trim($line);
+      
+      $space_pos = strrpos($line,' ');
 
-		//$otvet_pos = strrpos($line,'Ответ');
-    		//$test_lines[] =substr( $line, 0, $otvet_pos);
-	 	//echo $line1;substr(strrchr(
-		//$pos = strrchr($line1, ' ');
-		//echo $pos;
-		//$line1 = substr($line1, $pos);
-		//echo $line1;
-		//       найти последний пробел 
-		//обрезать строку до последнего пробела    // извлечь строку с таким номером
-		$line2 = $ls2[$num2];
-		$line3 = $ls3[$num3];
-		$line4 = $ls4[$num4];
-		$line5 = $ls5[$num5];
-		$line6 = $ls6[$num6];
-		$line7 = $ls7[$num7];
-		$line8 = $ls8[$num8];
-		$line9 = $ls9[$num9];
-		$line10 = $ls10[$num10];
-		$line11 = $ls11[$num11];
-		$line12 = $ls12[$num12];
-		$line13 = $ls13[$num13];
-		$line14 = $ls14[$num14];
-		$line15 = $ls15[$num15];
-		$line16 = $ls16[$num16];
-		$line17 = $ls17[$num17];
-		$line18 = $ls18[$num18];
-		$line19 = $ls19[$num19];
-		$line20 = $ls20[$num20];
-		$line21 = $ls21[$num21];
+        $true_answ = trim(substr( $line, $space_pos + 1));
+        //echo $true_answ.'<br>';
+        if( $true_answ == $answ_list[$num]){
+          $itog_count += 1;
+        }
+    }
+    
+    }
+    else {
+        $tst_nm = '';  //-- при первом посещении 
+    }
 
-	?>
+?>
+
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link href="tstyle.css" rel="stylesheet" type="text/css" />
-	
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link href="test.css" rel="stylesheet" type="text/css" />
+  <meta charset="utf-8">
 <title>Тест</title>
-</head>
-<html>
-<head>
-<title> </title>
-</head>
+<title>Меню</title>
 </head>
 
-<TR> <TD><img src="135152.jpg" width=1400 height=120</Td><TD></Td></TR>
 
+<TR> <TD><img src="135152.jpg" width=1400 height=120></Td></TR>
 
 <body BGCOLOR="#E6E6FA"> 
-<meta charset="utf-8">
+
 <title>Выравнивание по центру</title>
   <style>
    .text {
     text-align:  center;
    }
-  </style>
 
-<head>
-    <meta charset="utf-8">
-    <title>Меню</title>
-    <style>
-      #navbar ul{
+   #navbar ul{
         display: none;
         background-color: #f90;
         position: absolute;
@@ -165,67 +105,55 @@
       #navbar ul li { float: none; }
       #navbar li:hover { background-color: #f90; }
       #navbar ul li:hover { background-color: #666; }
-    </style>
-  </head>
- 
-  <body>
+      .text {
+        text-align: center;
+      }
+  </style>
+
+<body>
   
     <ul id="navbar">
-      <li><a href="index.html">Главная</a>
+      <li><a href="index.php">Главная</a>
       <ul>
           <li><a href="6.html">О нас</a></li>
         </ul>
       <li><a href="#">Личная информация</a></li>
-      <li><a href="#">Тесты</a>
-        <ul>
-          <li><a href="test.html">Начать тест</a></li>
-        </ul>
-       <li><a href="5.html">Вход</a></li>
-       <li><a href="4.html">Регистрация</a></li>
+      <li><a href="test_my.php">Тесты</a>
+       <li><a href="5.php">Вход</a></li>
+       <li><a href="4.php">Регистрация</a></li>
     </ul>
-  </body>
-<head>
 
-<title>Тест №1</title>
-<style>
-  
-.text {
-text-align: center;
-}
-</style>
-</head>
-<body>
-<body>
 <div class="text">
-<body BGCOLOR="#E6E6FA">
+
 <h2> Случайный вариант </h2>
 <blockquote>
 <hr> <B><h3>Вариант состоит из 21 задания. Ответом может быть целое число, десятичная дробь (записывайте её через запятую, вот так: 2,5) или последовательность цифр (пишите без пробелов: 97531).</h3></hr></B>
 </blockquote>
 </div>
-</body>
-</head>
-<body>
-	
-	<div class = all_tasks>
-		<?php echo $line1; ?> 
-		<td><input type = "text" id =line1 class = divtask> </td>
-		<p><?php echo $line2; ?>
-		<td><input type = "text" id =line2 class = divtask></td>
-		<p><?php echo $line3; ?>
-		<td><input type = "text" id =line3  class = divtask></td>
-		<p><?php echo $line4; ?>
-		<td><input type = "text" id =line4  class = divtask></td>
-		<p><?php echo $line5; ?>
-		<td><input type = "text" id =line5  class = divtask></td>
-	
-	<div id = divitog>
-		<input type="button" id = 'itog_btn' value="Подвести итог" >
-		<p></p>
-		<input type = "text" id = "itog_text">
-		<br>
-		Число правильных ответов: 
-		<input type = "text" id = "itog_count">
-	</div>	
+  
+  <div class = all_tasks>
+    <p>
+    <a href = test.php?fl_nm=01.txt>Задание 1</a>
+    <p> 
+    <a href = test.php?fl_nm=02.txt>Задание 2</a>
+    <p></p>
+  
+  <?php  if( $tst_nm != ''): ?>
+    <table class = results>
+      <tr>
+        <td>Пройден тест:</td> 
+        <td><input type = "text" id ="test_name" value = "<?php echo $tst_nm; ?>" ></td>
+      </tr><tr>
+        <td>Строка ответов: </td>
+        <td><input type = "text" id ="itog_text" value = "<?php echo $itog_text; ?>" ></td>
+      </tr><tr>
+        <td>Общее число вопросов: </td>
+        <td><input type = "text" class = "count_result" value = "<?php echo $quest_count; ?>" ></td>
+      </tr><tr>
+        <td>Число правильных ответов: </td>
+        <td><input type = "text" class = "count_result" value = "<?php echo $itog_count; ?>" ></td>
+      </tr>
+    </table>
+  <?php endif ?>    
 </body>
 </html>
