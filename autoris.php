542cbb9f8368d1ad9569754f58776e5f90296266
NastyaStaneva <86763482+NastyaStaneva@users.noreply.github.com>
2021-11-24 20:45:50 +0200

    Add files via upload

---
+++ autoris.php
@@ -0,0 +1,107 @@
+<?php 
+    if ( (isset($_COOKIE['auth']))&(!isset($_POST['unregist']))){
+        $role = $_COOKIE['auth'];
+        $registered = 1;  //уже был зарегистрирован
+    } 
+    else{
+        $registered = 0;
+		setcookie('auth', "");
+		setcookie('user_id', "");
+		setcookie('user_name', "");
+
+        $role = 'any'; $login=''; $pass='';
+        //проверяем правильность регистрации по форме
+        if (isset($_POST['email'])) 
+        { 
+            $login = $_POST['email']; 
+            $login = trim($login);
+        } 
+        if (isset($_POST['password'])) 
+        { 
+            $pass=$_POST['password']; 
+            $pass = trim($pass);
+        }
+        if( $login != ""){
+            //ищем логин и пароль в таблице пользователей
+            require ("dbconnect.php");
+            $sql = "SELECT * FROM users ";
+            $result = $conn->query($sql);
+            $result = mysqli_query($conn, $sql);
+            if (mysqli_num_rows($result) > 0) {
+                 //проверить каждую запись
+                 while($row = mysqli_fetch_assoc($result)) {
+                     if(( $row['email'] == $login) && ( $row['password'] == $pass) ){
+                         $registered = 1;
+                        // setcookie('auth', 'admin'); 
+						setcookie('auth', $row['role']);
+						setcookie('user_id', $row['id']);
+						setcookie('user_name', $row['firs_name'].' '.$row['last_name']);
+                        break;     
+                     }
+                 }
+            }
+            else{
+                 $registered = -1; //неверный логин и пароль
+            }
+        }
+    }
+?>        
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Вход</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body BGCOLOR="#E6E6FA">
+<TR> <TD><img src="135152.jpg" width=1395 height=120><TD></Td></TR>
+
+<ul id="navbar">
+      <li><a href="index.php">Главная</a>
+      <li><a href="information.html">Личная информация</a></li>
+      <li><a href="test_my.php">Тесты</a>
+      <li><a href="autoris.php">Вход</a></li>
+      <li><a href="registr.php">Регистрация</a></li> 
+    </ul>
+
+
+<!-- если удачно зарегистрировались -->
+<?php if( $registered == 1): ?>
+Вы правильно зашли<br>
+<a href='index.php'>вернуться на главную страницу</a>
+<form method=post>
+	<input type = hidden name = unregist value = 1>
+	<input type = submit value = "отменить авторизацию">
+</form>
+<?php  endif ?>
+
+
+<!-- если неверный логин или пароль -->
+<?php if( $registered == -1){
+    echo ("Извините, введённый вами логин или пароль неверный. <br>");    
+}
+?>
+
+<!-- если еше не зарегистрировались -->
+<?php if( $registered < 1): ?>
+
+<form method="POST" >
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+	<br>
+    
+    <label for="password"><b>Пароль</b></label>
+    <input type="password" placeholder="Enter Password" name="password" required>
+    <br>   
+    <button type="submit">Войти</button>
+
+</form>
+<br>
+<a href='index.php'>вернуться на главную страницу</a>
+<?php  endif ?>
+
+</body>
+</html>
---
+++ registr.php
@@ -0,0 +1,114 @@
+<?php
+  //регистрация. запись данных в бд
+  require('dbconnect.php');
+  $first_entry = 1;
+
+  if(isset($_POST['email']) && isset($_POST['password'])) {
+    $last_name = $_POST['last_name'];
+    $firs_name = $_POST['firs_name'];
+    $patronymic = $_POST['patronymic'];
+    $email = $_POST['email'];
+    $school = $_POST['school'];
+    $class = $_POST['class'];
+    $role = $_POST['role'];
+    $password = $_POST['password'];
+    $password_snd = $_POST['password_snd'];
+
+    $query = "INSERT INTO users ( last_name, firs_name, patronymic, email, school, class, role, password, password_snd) VALUES ( '$last_name','$firs_name', '$patronymic', '$email', '$school', '$class', '$role', '$password', '$password_snd')";
+
+    $res = $conn->query($query);
+    if($res) {
+        $ssmg = "Вы успешно зарегистрировались.";
+        $first_entry = 0;
+
+    }
+    else {
+        $fsmg = "Ошибка. Возможно, вы ввели некоретные данные или такой пользователь уже существует.";
+        die($fsmg);
+    }
+    
+  }
+  
+?> 
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="styles.css" rel="stylesheet" type="text/css">
+<title>Регистрация</title>
+</head>
+
+<TR> <TD><img src="135152.jpg" width=1400 height=120><TD></Td></TR>
+
+
+<body BGCOLOR="#E6E6FA"> 
+
+    <ul id="navbar">
+      <li><a href="index.php">Главная</a>
+      <li><a href="information.html">Личная информация</a></li>
+      <li><a href="test_my.php">Тесты</a>
+      <li><a href="autoris.php">Вход</a></li>
+      <li><a href="registr.php">Регистрация</a></li> 
+    </ul>
+
+<?php if ($first_entry == 1): ?>
+  <div class="container">
+    
+      <form class = "form-signin" method="POST"  >  
+          <h1>Регистрация</h1>
+          <p>Пожалуйста, заполните эту форму для создания аккаунта.</p>
+          <hr>
+
+        <?php if (isset($ssmg)){ ?> <div class = "alert alert-success" role = "alert"> <?php echo $ssmg;?> </div> <?php } ?><a href = "index.php">Перейти на главную</a><br>
+
+        <?php if (isset($fsmg)){ ?> <div class = "alert alert-danger" role = "alert"> <?php echo $fsmg;?> </div> <?php } ?>
+       
+
+          <label for="psw"><b>Фамилия</b></label>
+          <input type="text" placeholder="" name="last_name" required>
+       
+          <label for="psw"><b>Имя</b></label>
+          <input type="text" placeholder="" name="firs_name" required>
+
+          <label for="psw"><b>Отчество</b></label>
+          <input type="text" placeholder="" name="patronymic" required>
+       
+          <label for="email"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="email" required>
+
+          <label for="psw"><b>Школа</b></label>
+          <input type="text" placeholder="" name="school" required>
+       
+          <label for="psw"><b>Класс</b></label>
+          <input type="text" placeholder="" name="class" required>
+
+          <label for="psw"><b>Роль</b></label>
+          <input type="text" placeholder="Введите stud или prepod" name="role" required>
+
+          <label for="psw"><b>Пароль</b></label>
+          <input type="password" placeholder="Введите пароль" name="password" required>
+          
+
+          <label for="psw-repeat"><b>Повторите пароль</b></label>
+          <input type="password" placeholder="Повторите пароль" name="password_snd" required>
+       
+          <input type="hidden"  name="registr" value=1>
+         
+          <label>
+            <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px">  Запомнить меня
+          </label>
+          
+          <p>Создав учетную запись, вы соглашаетесь с нашими Условиями и Конфиденциальностью.<a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+          <div class="clearfix">
+              <button type="button"  class="cancelbtn">Отмена</button>
+              <button type="submit" name = "do_singup" class="signupbtn" value="submit">Зарегистрироваться</button>
+          </div>
+      </form>
+   </div>
+<?php else: ?>
+    <?php  echo $ssmg; ?> <br>
+    <a href = "index.php">Перейти на главную</a>
+<?php endif; ?>
+</body>
+</html>
